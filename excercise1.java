db10f9bf5c491ae55998306a303181009d59faeb
Jedsada-1339 <jedsada.si@kkumail.com>
2023-11-14 18:18:25 +0700

    d

---
+++ excercise1.java
@@ -0,0 +1,14 @@
+public class excercise1 {
+    public static void main(String[] args) {
+        String name ="Jedsada sirikhojornsombut";
+        double gpa =3.83;
+        boolean likePrograming = true;
+        System.out.println("My name is "+ name +" who has GPA as "+gpa);
+        if (likePrograming){
+            System.out.println("I love programing");
+        } else{
+            System.out.println("I don't like programing");
+        }
+
+    }
+}
