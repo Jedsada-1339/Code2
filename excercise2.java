5105837209c838a3bbf72bf6ae8f9349c487bff4
Jedsada-1339 <jedsada.si@kkumail.com>
2023-11-17 19:00:11 +0700

    cc

---
+++ excercise2.java
@@ -0,0 +1,9 @@
+public class excercise2 {
+    public static void main(String[] args) {
+        String name ="Jedsada";
+        String university_name="Khon Kaen University";
+        System.out.println("Hello, my name is "+name+".  "+"Now I study at"+university_name
+        );
+
+    }
+}
