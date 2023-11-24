d18bab4268993c827a78535b850387990a7ed820
Jedsada-1339 <jedsada.si@kkumail.com>
2023-11-24 14:16:08 +0700

    Go

---
+++ project/assets/js/theme.js
@@ -0,0 +1,26 @@
+
+"use strict";
+
+const /**Nodeelement */ $HTML = document.documentElement;
+const /**Boolean */ isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (sessionStorage.getItem("theme")){
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else{
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}
+
+let /**Bolean */ isPressed = false;
+
+const changetheme = function(){
+    isPressed = isPressed ? false : true;
+    this.setAttribute("aria-pressed",isPressed);
+    $HTML.setAttribute("data-theme", ($HTML.dataset.theme === "light") ?"dark" : "light");
+    sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+window.addEventListener("load",function(){
+    const /**Nodeelement */ $themeBtn = document.querySelector("[data-theme-btn]");
+
+    $themeBtn.addEventListener("click", changetheme);
+});
