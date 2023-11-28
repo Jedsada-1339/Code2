6a5c5e3f07eecaf880f50c27444d38dbcb80b030
Jedsada-1339 <jedsada.si@kkumail.com>
2023-11-28 11:56:15 +0700

    ีupdate

--- project/assets/js/home.js
+++ project/assets/js/home.js
@@ -1,7 +1,13 @@
-/**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
 
 "use strict";
+
+/**
+ * Home page Search
+ */
+
+const /** {NodeElement} */ $searchField = document.querySelector("[data-search-field]");
+const /** {NodeElement} */ $searchBtn = document.querySelector("[data-search-btn]");
+
+$searchBtn.addEventListener("click" ,function(){
+    if ($searchField.value) window.location = `/recipes.html?q=${$searchField.value}`;
+});
