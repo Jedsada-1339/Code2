fd53bc6883246005ba5b89d7fa466e3d11b957c4
Jedsada-1339 <jedsada.si@kkumail.com>
2023-11-28 21:58:52 +0700

    update

--- project/assets/js/global.js
+++ project/assets/js/global.js
@@ -1,7 +1,8 @@
-/**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
 
 "use strict";
+
+window.addEventOnElement = ($elements, eventType, callback) => {
+    for (const $element of $elements) {
+        $element.addEventListener(eventType, callback);
+    }
+}
--- project/assets/js/home.js
+++ project/assets/js/home.js
@@ -11,3 +11,36 @@ const /** {NodeElement} */ $searchBtn = document.querySelector("[data-search-btn
 $searchBtn.addEventListener("click" ,function(){
     if ($searchField.value) window.location = `/recipes.html?q=${$searchField.value}`;
 });
+
+/**
+ * Search submit when press "Enter" key
+ */
+
+$searchField.addEventListener("keydown", e => {
+    if (e.key === "Enter") $searchBtn.click();
+})
+
+/**
+ * Tab panel navigation
+ */
+
+const /** {Nodelist} */ $tabBtns = document.querySelectorAll("[data-tab-btn]");
+const /** {Nodelist} */ $tabPanels = document.querySelectorAll("[data-tab-panel]");
+
+let /** NodeElement */ [$lastActiveTabPanel] = $tabPanels;
+let /** NodeElement */ [$lastActiveTabBtn] = $tabBtns;
+
+addEventOnElement($tabBtns, "click", function(){
+    $lastActiveTabPanel.setAttribute("hidden","");
+    $lastActiveTabBtn.setAttribute("aria-selected", false);
+    $lastActiveTabBtn.setAttribute("tabindex", -1);
+
+    const /** {NodeElement} */ $currentTabPanel = document.querySelector
+    (`#${this.getAttribute("aria-controls")}`);
+    $currentTabPanel.removeAttribute("hidden");
+    this.setAttribute("aria-selected", true);
+    this.setAttribute("tabindex", 0);
+
+    $lastActiveTabPanel = $currentTabPanel;
+    $lastActiveTabBtn = this;
+});
