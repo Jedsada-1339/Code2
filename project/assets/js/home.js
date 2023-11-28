818c76f825779ae10bf173b56378f220f2f1093a
Jedsada-1339 <jedsada.si@kkumail.com>
2023-11-29 00:01:53 +0700

    update

--- project/assets/js/home.js
+++ project/assets/js/home.js
@@ -62,7 +62,7 @@ addEventOnElement($tabBtns, "keydown", function (e){
         this.setAttribute("tabindex", -1);
         $previousElement.setAttribute("tabindex", 0);
         $previousElement.focus()
-    } else if (e.key === "Tab") {
+    } else if (e.key === "ArrowUp") {
         this.setAttribute("tabindex", -1);
         $lastActiveTabBtn.setAttribute("tabindex", 0);
     }
