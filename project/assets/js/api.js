b38cd7efca2b96c5884f74c8676cff6c777143c7
Jedsada-1339 <jedsada.si@kkumail.com>
2023-11-30 17:32:14 +0700

    updata footer

--- project/assets/js/api.js
+++ project/assets/js/api.js
@@ -1,7 +1,24 @@
-/**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
 
 "use strict";
+
+window.ACCESS_POINT = "https://api.edamam.com/api/recipes/v2";
+const /** {String} */ APP_ID = "22215f11";
+const /** {String} */ API_KEY = "0d89a6c87bba49102fb456cbe4645de2";
+const /** {String} */ TYPE = "public";
+
+export const fetchdata = async function (queries, successCallback) {
+    const /** {String} */ query = queries?.join("&")
+        .replace(/,/g, "=")
+        .replace(/ /g, "%20")
+        .replace(/\+/g, "%2B");
+
+    const /** {String} */ url = `${ACCESS_POINT}?app_id=$
+    {APP_ID}&app_key=${API_KEY}&type=${TYPE}${query ? `&${query}` : ""}`;
+
+    const /** {Object} */ reponse = await fetch(url);
+
+    if (reponse.ok) {
+        const data = await reponse.json();
+        successCallback(data);
+    }
+}
