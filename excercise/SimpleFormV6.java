39f31e71eaaa5a0bda6634ec5359a228f5f4d9f8
Jedsada-1339 <jedsada.si@kkumail.com>
2024-02-20 17:47:57 +0700

    update

--- excercise/SimpleFormV6.java
+++ excercise/SimpleFormV6.java
@@ -1,38 +1,52 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.File;
 import java.util.*;
 import javax.swing.event.*;
-import java.awt.*; // Add this import statement for BorderLayout
+import java.awt.*;
 
-public class SimpleFormV6 extends SimpleFormV5 implements ActionListener, ItemListener {
-    JPanel genderPanel,codeLanguagesPanel;
+public class SimpleFormV6 extends SimpleFormV5 implements ActionListener{
+    JPanel genderPanel, codeLanguagesPanel;
+    JFileChooser fileChooser; // Fix the variable name
+    File file;
+
     public SimpleFormV6(String name) {
         super(name);
     }
 
     public void addComponents() {
         super.addComponents();
-
     }
 
     public void addListeners() {
         super.addListeners();
+        openMI.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent e) {
-        super.actionPerformed(e);   
+        super.actionPerformed(e);
         Object srcObject = e.getSource();
         if (srcObject == okButton) {
             String name = nameTxt.getText();
             String addr = addrTxtArea.getText();
             JOptionPane.showMessageDialog(this, name + " teaches at " + addr);
-        }else if(srcObject == cancelButton){
+        } else if (srcObject == cancelButton) {
             nameTxt.setText("");
             addrTxtArea.setText("");
         }
-    }
 
-    public void itemStateChanged(ItemEvent e) {
+        fileChooser = new JFileChooser();
+
+        if (srcObject == openMI) {
+            int returnVal = fileChooser.showOpenDialog(this); // Fix the variable name
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                file = fileChooser.getSelectedFile();
+                JOptionPane.showMessageDialog(this, "Opening file " + file.getName());
+            } else {
+                JOptionPane.showMessageDialog(this, "Open file cancelled by user");
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -46,7 +60,7 @@ public class SimpleFormV6 extends SimpleFormV5 implements ActionListener, ItemLi
     public static void createAndShowGUI() {
         SimpleFormV6 window = new SimpleFormV6("SimpleFormV6");
         window.addComponents();
-        window.addMenus();
+        window.addMenus(); // Ensure you have the addMenus() method
         window.addListeners();
         window.setFrameFeatures();
     }
