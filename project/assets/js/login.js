f4cd6d11749f9680c4540eb0ecdda02a46300372
Piyabud <piyabud.d@kkumail.com>
2024-02-20 18:55:06 +0700

    updated login page

--- project/assets/js/login.js
+++ project/assets/js/login.js
@@ -3,8 +3,10 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.item-close');
-const iconvisibility = document.querySelector('.visibility');
-const iconvisibilityoff = document.querySelector('.visibilityoff');
+const iconvisibility = document.querySelector('.form-box.login .visibility');
+const iconvisibilityoff = document.querySelector('.form-box.login .visibilityoff');
+const iconrevisibility = document.querySelector('.form-box.register .visibility');
+const iconrevisibilityoff = document.querySelector('.form-box.register .visibilityoff');
 
 registerLink.addEventListener("click", ()=> {
     wrapper.classList.add('active');
@@ -35,6 +37,24 @@ iconvisibilityoff.addEventListener("click", ()=> {
     wrapper.classList.remove('active-open');
     
     let passwordInput = iconvisibilityoff.parentElement.querySelector("input");
+    if (passwordInput.type === "text") {
+        passwordInput.type = "password";
+    }
+})
+
+iconrevisibility.addEventListener("click", ()=> {
+    wrapper.classList.add('active-open');
+
+    let passwordInput = iconrevisibility.parentElement.querySelector("input");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    }
+})
+
+iconrevisibilityoff.addEventListener("click", ()=> {
+    wrapper.classList.remove('active-open');
+    
+    let passwordInput = iconrevisibilityoff.parentElement.querySelector("input");
     if (passwordInput.type === "text") {
         passwordInput.type = "password";
     }
