49f07d8475b9095116f3942925ac8f2169259551
Jedsada-1339 <jedsada.si@kkumail.com>
2024-03-07 14:47:47 +0700

    update

--- project/assets/js/saved_recipes.js
+++ project/assets/js/saved_recipes.js
@@ -31,7 +31,7 @@ if (savedRecipes.length) {
     } = JSON.parse(window.localStorage.getItem(savedRecipe));
 
     const /** {String} */ recipeId = uri.slice(uri.lastIndexOf("_") + 1);
-    const /** {undefined || String} */ isSaved = window.localStorage.getItem(`cookio-recipe${recipeId}`);
+    const /** {undefined || String} */ isSaved = window.localStorage.getItem(`FoodLover-recipe${recipeId}`);
 
     const /** {NodeElement} */ $card = document.createElement("div");
     $card.classList.add("card");
@@ -57,11 +57,13 @@ if (savedRecipes.length) {
             <span class="label-medium">${getTime(cookingTime).time || "<1"} ${getTime(cookingTime).timeUnit}</span>
           </div>
 
-          <button class="icon-btn has-state ${isSaved ? "saved" : "removed"}" aria-label="Add to saved recipes" onclick="saveRecipe(this, '${recipeId}')">
-            <span class="material-symbols-outlined bookmark-add" aria-hidden="true">bookmark_add</span>
+        <button class="icon-btn has-state ${isSaved ? "saved" : "removed"}" aria-label="Add to saved recipes" onclick="saveRecipe(this, '${recipeId}')">
 
-            <span class="material-symbols-outlined bookmark" aria-hidden="true">bookmark</span>
-          </button>
+            <span class="material-symbols-outlined bookmark" aria-hidden="true">bookmark_add</span>
+            
+            <span class="material-symbols-outlined bookmark-add" aria-hidden="true">bookmark</span>
+
+        </button>
 
         </div>
 
