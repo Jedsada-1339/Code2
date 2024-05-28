bb6a15edb8fd381af79cfe7da7f21f0251b80bc4
Jedsada-1339 <jedsada.si@kkumail.com>
2024-05-28 21:32:46 +0700

    update

---
+++ excercise/rethink.java
@@ -0,0 +1,19 @@
+public class rethink {
+    
+    public static String name;
+    public static void main(String[] args) {
+        name = "Dove";
+        System.out.println(name);
+
+        if (name.equals("Dove")){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fales");
+        }
+        int number = 2;
+        ++number;
+        System.out.println(number);
+        // System.out.println(++number);
+    }
+}
